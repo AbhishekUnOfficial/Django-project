55d8a6dbffa569c4d3fba53d0aa2b3d489b7cf9b
AbhishekUnOfficial <avishek.ov@gmail.com>
2023-06-06 09:31:40 +0530

    wsgi update

--- webscraper/wsgi.py
+++ webscraper/wsgi.py
@@ -1,23 +1,3 @@
-"""
-WSGI config for webscraper project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-"""
-# below part is disable to deploy on vercel
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webscraper.settings")
-
-application = get_wsgi_application()
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -26,4 +6,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webscraper.settings")
 
 application = get_wsgi_application()
 
-app = application
+app = application # for vercel
