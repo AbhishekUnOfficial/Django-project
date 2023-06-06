1f1b46f8da0c63d6ba9d6e97b29ad4dd4a61f450
Abhishek Mallick <78242724+AbhishekUnOfficial@users.noreply.github.com>
2023-06-06 09:29:11 +0530

    Update wsgi.py

--- webscraper/wsgi.py
+++ webscraper/wsgi.py
@@ -6,8 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-# disable this part to deploy on vercel
 """
+# below part is disable to deploy on vercel
+
 import os
 
 from django.core.wsgi import get_wsgi_application
