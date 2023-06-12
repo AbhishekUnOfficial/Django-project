cb926c702eb0b44e1e714a242ae3785958712d1a
AbhishekUnOfficial <avishek.ov@gmail.com>
2023-06-12 19:02:24 +0530

    updated

--- scraper/static/script.js
+++
@@ -1,9 +0,0 @@
-function setLink(event, element) {
-  event.preventDefault(); // Disable default link behavior
-  var link = element.getAttribute("href");
-  document.getElementById("link").value = link;
-}
-
-function clearInput() {
-  document.getElementById("link").value = "";
-}
